366ebdc848fa01126cdc3760bcf8c41f5031007b
Vishal Wadhwa <vishalwadhwa13@gmail.com>
2019-01-26 17:37:44 +0530

    added logo component. AppLogo.vue

---
+++ src/components/AppLogo.vue
@@ -0,0 +1,35 @@
+<template>
+  <span :style='styling' style='border-radius:15px; padding: 1px; border-bottom: 3px double;'>
+    <strong>{{mini ? 'T' : 'TODO'}}</strong>
+    <em class='font-weight-thin'>Board</em>
+  </span>
+</template>
+<script>
+export default {
+  props: {
+    size: {
+      type: [String, Number],
+      default: 14
+    },
+    color: {
+      type: String,
+      default: 'white'
+    },
+    mini: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    styling () {
+      return {
+        fontSize: this.isNumericSize ? this.size + 'px' : this.size,
+        color: this.color
+      }
+    },
+    isNumericSize () {
+      return !isNaN(parseFloat(this.size)) && isFinite(this.size)
+    }
+  }
+}
+</script>

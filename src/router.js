bcaabef5b2e2ec73c332954c76df2053247d6cd5
Vishal Wadhwa <vishalwadhwa13@gmail.com>
2019-02-03 19:42:08 +0530

    removed about.vue and its route

--- src/router.js
+++ src/router.js
@@ -26,14 +26,6 @@ const router = new Router({
           })
         } else next()
       }
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
     }
   ]
 })

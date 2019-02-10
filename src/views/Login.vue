9e798eaaa327bba6877b60a59cbc75a0a8fb2942
Vishal Wadhwa <vishalwadhwa13@gmail.com>
2019-02-10 22:49:22 +0530

    added sso buttons to login view

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -10,7 +10,7 @@
       <v-layout row fill-height class='login-view__content'>
         <app-logo class='logo' color='black' size='1.5rem'></app-logo>
         <v-flex class='login-fields-container' md4 xs12>
-          <v-form class='mx-4' style='width: 100%;' ref='form'>
+          <v-form class='mx-4 mt-5' style='width: 100%;' ref='form'>
             <v-text-field
               v-model.trim='fullname'
               color='black'
@@ -19,6 +19,7 @@
               :rules='rules.fullname'
               validate-on-blur
               ref='fullNameField'
+              autofocus
               v-if='curState === STATE_SIGN_UP'
             ></v-text-field>
             <v-text-field
@@ -43,15 +44,21 @@
               @click:append='showPass = !showPass'
             ></v-text-field>
             <v-layout class='mt-3' justify-space-between>
-              <div></div>
-              <div>
-                <span @click='toggleState' class='btn-create-acct body-1'>
-                  {{
-                  curState === STATE_SIGN_IN ?
-                  'Create account?' :
-                  'Login?'
-                  }}
-                </span>
+              <v-layout>
+                <o-auth-login-btn
+                  :icon-src='require("@/assets/svg/gsearch_avatar.svg")'
+                  title='Google'
+                  text-color='#757575'
+                  btn-bg='white'
+                ></o-auth-login-btn>
+                <o-auth-login-btn
+                  :icon-src='require("@/assets/svg/guest_avatar.svg")'
+                  title='Guest'
+                  btn-bg='rgba(244, 180, 0, 0.5)'
+                  icon-bg='rgba(244, 180, 0, 1)'
+                ></o-auth-login-btn>
+              </v-layout>
+              <v-layout align-end column>
                 <v-btn
                   dark
                   round
@@ -59,7 +66,12 @@
                   @click='onSubmit'
                   :loading='authenticating'
                 >{{curState === STATE_SIGN_IN ? 'LogIn' : 'SignUp'}}</v-btn>
-              </div>
+                <span
+                  @click='toggleState'
+                  class='btn-create-acct body-1 text-xs-center'
+                  v-html='curState === STATE_SIGN_IN ? "Create<br>account?" : "Login?"'
+                ></span>
+              </v-layout>
             </v-layout>
           </v-form>
         </v-flex>
@@ -75,12 +87,14 @@
 <script>
 // https://dribbble.com/shots/3586598-Bank-login-panel
 import AppLogo from '../components/AppLogo'
+import OAuthLoginBtn from '../components/OAuthLoginBtn'
+
 import { mapActions } from 'vuex'
 const STATE_SIGN_UP = 'SIGN_UP'
 const STATE_SIGN_IN = 'SIGN_IN'
 
 export default {
-  components: { AppLogo },
+  components: { AppLogo, OAuthLoginBtn },
   data () {
     return {
       STATE_SIGN_UP: STATE_SIGN_UP,
@@ -91,9 +105,9 @@ export default {
       fullname: '',
       showPass: false,
       rules: {
-        email: [val => /\S+@\S+\.\S+/.test(val.trim().toLowerCase()) || 'Please enter a valid email ID'],
-        password: [val => val.length >= 8 || 'Password must have a minimum of 8 letters'],
-        fullname: [val => !!val.trim().length || 'Please enter your full name']
+        email: [val => (!!val && /\S+@\S+\.\S+/.test(val.trim().toLowerCase())) || 'Please enter a valid email ID'],
+        password: [val => (!!val && val.length) >= 8 || 'Password must have a minimum of 8 letters'],
+        fullname: [val => (!!val && !!val.trim().length) || 'Please enter your full name']
       },
       authenticating: false
     }
@@ -102,8 +116,9 @@ export default {
     toggleState () {
       this.curState = this.curState === this.STATE_SIGN_UP ? this.STATE_SIGN_IN : this.STATE_SIGN_UP
       this.$refs.form.reset()
-      if (this.curState === this.STATE_SIGN_UP) this.$nextTick(this.$refs.fullNameField.focus)
-      else this.$nextTick(this.$refs.emailField.focus)
+      // no need to call focus for full name field bcz it is on autofocus on mount
+      // it is remounted everytime bcz form validation should not apply to it when sign in state
+      if (this.curState === this.STATE_SIGN_IN) this.$nextTick(this.$refs.emailField.focus)
     },
     async onSubmit (ev) {
       if (!this.$refs.form.validate()) return

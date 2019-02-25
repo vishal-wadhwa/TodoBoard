cc35524e715da22acf2013c6ea63676fa3266246
Vishal Wadhwa <vishalwadhwa13@gmail.com>
2019-02-25 19:10:49 +0530

    feature: Notify.vue component for notifications

---
+++ src/components/Notify.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-snackbar v-show='!!msg' v-model='snackbar' bottom auto-height>
+    <v-icon :color='type' class='mr-3' medium>{{TYPE_TO_ICON[type]}}</v-icon>
+    {{msg}}
+    <v-btn color='pink' flat @click='snackbar = false'>Close</v-btn>
+  </v-snackbar>
+</template>
+<script>
+const TYPES = ['success', 'info', 'warning', 'error']
+
+export default {
+  props: {
+    msg: String,
+    type: {
+      type: String,
+      default: 'info',
+      validator: v => TYPES.includes(v)
+    }
+  },
+  data () {
+    return {
+      snackbar: false,
+      TYPE_TO_ICON: {
+        success: 'check_circle',
+        info: 'info',
+        warning: 'warning',
+        error: 'error'
+      }
+    }
+  },
+  created () {
+    this.snackbar = !!this.msg
+  },
+  watch: {
+    msg (nv) {
+      this.snackbar = true
+    }
+  }
+}
+</script>

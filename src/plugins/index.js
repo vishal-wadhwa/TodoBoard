9335c53163fcb879ec7682ba338ca3fce256a920
Vishal Wadhwa <vishalwadhwa13@gmail.com>
2019-02-26 11:59:09 +0530

    change: all plugins now imported from single index.js

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify/lib'
 import 'vuetify/src/stylus/app.styl'
+import Notify from './Notify'
 
 Vue.use(Vuetify, {
   iconfont: 'md'
 })
+
+Vue.use(Notify)

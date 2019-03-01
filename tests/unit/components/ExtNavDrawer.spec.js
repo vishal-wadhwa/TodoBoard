a9296e8f741794e2a8fe7aed9d735d6ca59ecfea
Vishal Wadhwa <vishalwadhwa13@gmail.com>
2019-03-02 02:11:38 +0530

    Update: added delete button tests for ExtNavDrawer.vue; Corrected previously incorrect tests

--- tests/unit/components/ExtNavDrawer.spec.js
+++ tests/unit/components/ExtNavDrawer.spec.js
@@ -3,6 +3,7 @@ import ExtNavDrawer from '@/components/ExtNavDrawer'
 import Vuetify from 'vuetify'
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+
 Vue.use(Vuetify)
 
 describe('ExtNavDrawer.vue', () => {
@@ -33,39 +34,118 @@ describe('ExtNavDrawer.vue', () => {
   ]
   const newBoardName = 'my-board-1234'
 
-  it('renders the board names correctly', () => {
+  it('renders the board names with titles correctly', () => {
     const wrapper = factory({
       propsData: { boardList },
       stubs: ['v-list-tile-title'],
       localVue
     })
 
-    const listItems = wrapper.findAll('vlisttile-stub')
+    const listItems = wrapper.findAll({ name: 'v-list-tile' })
 
     expect(listItems.length).toBe(boardList.length + 1)
     for (let i = 0; i < boardList.length; ++i) {
       expect(listItems.at(1 + i).text()).toMatch(boardList[i].boardName)
+      expect(listItems.at(i + 1).attributes('title')).toBe(boardList[i].boardName)
     }
   })
 
-  it('updates the list view when a new board is added', () => {
+  it('deletes the key of the board on which delete is triggered', async () => {
+    const wrapper = factory({
+      propsData: { boardList },
+      stubs: ['router-link'],
+      localVue,
+      sync: false
+    }, true)
+
+    const delButton = wrapper.find({ name: 'v-btn' })
+    const listItem = wrapper.find('.v-list__tile[title]')
+
+    expect(wrapper.vm.deleteBtnVisible[boardList[0]._id]).toBeUndefined()
+
+    listItem.trigger('mouseover')
+    await wrapper.vm.$nextTick()
+
+    expect(wrapper.vm.deleteBtnVisible[boardList[0]._id]).toBeTruthy()
+
+    delButton.trigger('click')
+    await wrapper.vm.$nextTick()
+
+    expect(wrapper.vm.deleteBtnVisible[boardList[0]._id]).toBeUndefined()
+  })
+
+  it('mouseover/mouseout toggle visibility of deleteButton', async () => {
+    const wrapper = factory({
+      propsData: { boardList },
+      stubs: ['router-link'],
+      localVue,
+      sync: false
+    }, true)
+
+    const delButton = wrapper.find({ name: 'v-btn' })
+    // const listItem = wrapper.findAll({ name: 'v-list-tile' }).at(1)
+    const listItem = wrapper.find('.v-list__tile[title]')
+
+    expect(delButton.isVisible()).toBeFalsy()
+
+    listItem.trigger('mouseover')
+    await wrapper.vm.$nextTick()
+
+    expect(delButton.isVisible()).toBeTruthy()
+
+    listItem.trigger('mouseout')
+    await wrapper.vm.$nextTick()
+
+    expect(delButton.isVisible()).toBeFalsy()
+  })
+
+  it('updates the list view when a new board is added', async () => {
     const boardItem = { _id: 4, boardName: '___34rfa45#$$@__aefq4' }
     const boardListCopy = [...boardList]
 
     const wrapper = factory({
       propsData: { boardList: boardListCopy },
-      stubs: ['v-list-tile-title'],
-      localVue
-    })
+      localVue,
+      stubs: ['router-link'],
+      sync: false
+    }, true)
 
-    const list = wrapper.find('vlist-stub')
+    const list = wrapper.find({ name: 'v-list' })
     expect(list.text()).not.toMatch(boardItem.boardName)
 
     boardListCopy.push(boardItem)
-    setTimeout(() => expect(list.text()).toMatch(boardItem.boardName), 0)
+
+    await wrapper.vm.$nextTick()
+
+    expect(wrapper.props('boardList')[3]).toEqual(boardItem)
+    expect(list.findAll({ name: 'v-list-tile' }).length).toBe(boardListCopy.length + 1)
+    expect(list.text()).toMatch(boardItem.boardName)
   })
 
-  it('displays text-field for new board only after it is toggled', () => {
+  it('updates the list view when a board is deleted', async () => {
+    const boardListCopy = [...boardList]
+
+    const wrapper = factory({
+      propsData: { boardList: boardListCopy },
+      stubs: ['router-link'],
+      localVue,
+      sync: false
+    }, true)
+
+    const list = wrapper.find({ name: 'v-list' })
+    const boardNameToDelete = boardListCopy[0].boardName
+
+    expect(list.text()).toMatch(boardNameToDelete)
+
+    boardListCopy.splice(0, 1)
+
+    await wrapper.vm.$nextTick()
+
+    expect(wrapper.props('boardList')[0].boardName).not.toMatch(boardNameToDelete)
+    expect(list.text()).not.toMatch(boardNameToDelete)
+  })
+
+  it('displays text-field for new board only after it is toggled', async () => {
     const wrapper = factory({
       localVue,
       sync: false
@@ -73,7 +153,9 @@ describe('ExtNavDrawer.vue', () => {
 
     expect(wrapper.find({ name: 'v-text-field' }).isVisible()).toBeFalsy()
     wrapper.setProps({ showNewFieldBox: true })
-    setTimeout(() => expect(wrapper.find({ name: 'v-text-field' }).isVisible()).toBeTruthy(), 0)
+
+    await wrapper.vm.$nextTick()
+    expect(wrapper.find({ name: 'v-text-field' }).isVisible()).toBeTruthy()
   })
 
   it('has appropriate "to" property attached to list items', () => {
@@ -95,7 +177,7 @@ describe('ExtNavDrawer.vue', () => {
     }
   })
 
-  it('emits "end:board-click" when a list item is clicked', () => {
+  it('emits "end:board-click" when a list item is clicked', async () => {
     const wrapper = factory({
       propsData: { boardList },
       stubs: ['router-link'],
@@ -103,97 +185,150 @@ describe('ExtNavDrawer.vue', () => {
       sync: false
     }, true)
 
-    const listItems = wrapper.findAll({ name: 'v-list-tile' })
-    listItems.at(1).trigger('click')
-    listItems.at(3).trigger('click')
+    const listItems = wrapper.findAll('.v-list__tile[title]')
+    listItems.at(0).trigger('click')
+    listItems.at(2).trigger('click')
 
-    setTimeout(() => {
-      const emitArr = wrapper.emitted('end:board-click')
+    await wrapper.vm.$nextTick()
 
-      expect(emitArr.length).toBe(2)
+    const emitArr = wrapper.emitted('end:board-click')
 
-      expect(emitArr[0].length).toBe(2)
-      expect(emitArr[0][0]).toEqual(boardList[0])
-      expect(emitArr[0][1]).toBeInstanceOf(Event)
+    expect(emitArr.length).toBe(2)
 
-      expect(emitArr[1].length).toBe(2)
-      expect(emitArr[1][0]).toEqual(boardList[2])
-      expect(emitArr[1][1]).toBeInstanceOf(Event)
-    }, 0)
+    expect(emitArr[0].length).toBe(2)
+    expect(emitArr[0][0]).toEqual(boardList[0])
+    expect(emitArr[0][1]).toBeInstanceOf(Event)
+
+    expect(emitArr[1].length).toBe(2)
+    expect(emitArr[1][0]).toEqual(boardList[2])
+    expect(emitArr[1][1]).toBeInstanceOf(Event)
   })
 
-  it('emits "end:bname-save" when enter-key is pressed', () => {
+  it('emits "end:board-delete" when delete button is clicked', async () => {
+    const wrapper = factory({
+      propsData: { boardList },
+      stubs: ['router-link'],
+      localVue,
+      sync: false
+    }, true)
+
+    const delButtons = wrapper.findAll('.v-btn')
+    delButtons.at(0).trigger('click')
+    delButtons.at(2).trigger('click')
+
+    await wrapper.vm.$nextTick()
+
+    const emitArr = wrapper.emitted('end:board-delete')
+
+    expect(emitArr.length).toBe(2)
+
+    expect(emitArr[0].length).toBe(2)
+    expect(emitArr[0][0]).toEqual(boardList[0])
+    expect(emitArr[0][1]).toBeInstanceOf(Event)
+
+    expect(emitArr[1].length).toBe(2)
+    expect(emitArr[1][0]).toEqual(boardList[2])
+    expect(emitArr[1][1]).toBeInstanceOf(Event)
+  })
+
+  it('emits "end:bname-save" when enter-key is pressed', async () => {
     const wrapper = factory({
       localVue,
+      propsData: { showNewFieldBox: true },
       sync: false
     }, true)
 
     wrapper.setData({ newBoardName })
-    const textField = wrapper.find({ name: 'v-text-field' })
-    textField.trigger('keydown', { key: 'Enter' })
 
-    setTimeout(() => {
-      const emitArr = wrapper.emitted('end:bname-save')
-      expect(emitArr.length).toBe(1)
-      expect(emitArr[0].length).toBe(2)
-      expect(emitArr[0][0]).toBe(newBoardName)
-      expect(emitArr[0][1]).toBeInstanceOf(Event)
-    }, 0)
+    const textField = wrapper.find('input')
+    textField.trigger('keydown.enter')
+
+    await wrapper.vm.$nextTick()
+
+    const emitArr = wrapper.emitted('end:bname-save')
+    expect(emitArr.length).toBe(1)
+    expect(emitArr[0].length).toBe(2)
+    expect(emitArr[0][0]).toBe(newBoardName)
+    expect(emitArr[0][1]).toBeInstanceOf(Event)
   })
 
-  it('emits "end:bname-save" when tick icon is clicked', () => {
+  it('emits "end:bname-save" when tick icon is clicked', async () => {
     const wrapper = factory({
       localVue,
+      propsData: { showNewFieldBox: true },
       sync: false
     }, true)
 
     wrapper.setData({ newBoardName })
-    const icon = wrapper.find({ name: 'v-text-field' }).find({ name: 'v-icon' })
-    icon.trigger('click')
 
-    setTimeout(() => {
-      const emitArr = wrapper.emitted('end:bname-save')
-      expect(emitArr.length).toBe(1)
-      expect(emitArr[0].length).toBe(2)
-      expect(emitArr[0][0]).toBe(newBoardName)
-      expect(emitArr[0][1]).toBeInstanceOf(Event)
-    }, 0)
+    const icon = wrapper.find('input')
+    icon.trigger('click:append-outer')
+
+    await wrapper.vm.$nextTick()
+
+    const emitArr = wrapper.emitted('end:bname-save')
+    expect(emitArr.length).toBe(1)
+    expect(emitArr[0].length).toBe(2)
+    expect(emitArr[0][0]).toBe(newBoardName)
+    expect(emitArr[0][1]).toBeInstanceOf(Event)
   })
 
-  it('emits "end:bname-discard" when esc-key is pressed', () => {
+  it('emits "end:bname-discard" when esc-key is pressed', async () => {
     const wrapper = factory({
       localVue,
+      propsData: { showNewFieldBox: true },
       sync: false
     }, true)
 
     wrapper.setData({ newBoardName })
-    const textField = wrapper.find({ name: 'v-text-field' })
-    textField.trigger('keydown', { key: 'Escape' })
 
-    setTimeout(() => {
-      const emitArr = wrapper.emitted('end:bname-save')
-      expect(emitArr.length).toBe(1)
-      expect(emitArr[0].length).toBe(1)
-      expect(emitArr[0][0]).toBeInstanceOf(Event)
-    }, 0)
+    const textField = wrapper.find('input')
+    textField.trigger('keydown.esc')
+
+    await wrapper.vm.$nextTick()
+
+    const emitArr = wrapper.emitted('end:bname-discard')
+    expect(emitArr.length).toBe(1)
+    expect(emitArr[0].length).toBe(1)
+    expect(emitArr[0][0]).toBeInstanceOf(Event)
   })
 
-  it('does not emit "end:bname-save" and goes into error state when boardName is empty', () => {
+  it('emits "end:blur" on textField blur', async () => {
     const wrapper = factory({
       localVue,
+      propsData: { showNewFieldBox: true },
       sync: false
     }, true)
 
-    const textField = wrapper.find({ name: 'v-text-field' })
-    textField.trigger('keydown', { key: 'Enter' })
+    const textField = wrapper.find('input')
+    textField.trigger('blur')
 
-    expect(wrapper.vm.$data.errMsg).toBe('')
+    await wrapper.vm.$nextTick()
 
-    setTimeout(() => {
-      const emitArr = wrapper.emitted('end:bname-save')
-      expect(emitArr.length).toBe(0)
-      expect(wrapper.vm.$data.errMsg).toBe(wrapper.props('ERROR_MSG'))
-    }, 0)
+    const emitArr = wrapper.emitted('end:blur')
+    expect(emitArr.length).toBe(1)
+    expect(emitArr[0].length).toBe(1)
+    expect(emitArr[0][0]).toBeInstanceOf(Event)
+  })
+
+  it('does not emit "end:bname-save" and goes into error state when boardName is empty', async () => {
+    const wrapper = factory({
+      localVue,
+      propsData: { showNewFieldBox: true },
+      sync: false
+    }, true)
+
+    const textField = wrapper.find('input')
+
+    expect(wrapper.vm.errMsg).toBe('')
+
+    textField.trigger('keydown.enter')
+
+    await wrapper.vm.$nextTick()
+
+    const emitArr = wrapper.emitted('end:bname-save')
+    expect(emitArr).toBeUndefined()
+    expect(wrapper.vm.errMsg).toBe(wrapper.vm.ERROR_MSG)
   })
 
   it('should match snapshot when text field is not visible', () => {
@@ -215,6 +350,23 @@ describe('ExtNavDrawer.vue', () => {
       sync: false
     }, true)
     wrapper.setData({ newBoardName })
+
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+
+  it('should match snapshot when delete button is visible', async () => {
+    const wrapper = factory({
+      localVue,
+      propsData: { boardList },
+      stubs: ['router-link'],
+      sync: false
+    }, true)
+    wrapper.setData({ newBoardName })
+
+    const listItem = wrapper.find('.v-list__tile[title]')
+    listItem.trigger('mouseover')
+
+    await wrapper.vm.$nextTick()
 
     expect(wrapper.html()).toMatchSnapshot()
   })
